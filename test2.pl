e047dc62abf61c3867c8f2db7fef712a1839e842
keith <>
2005-11-11 15:42:30 +0000

    Removed old scripts

--- test2.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/perl5.6.0 -w
-
-use Ace;
-
-my $db = Ace->connect (-path => "/wormsrv2/autoace", -program => "/nfs/disk100/acedb/RELEASE.DEVELOPMENT/bin.ALPHA_5/tace");
-
-print "Hello\n";   
-#$db->close;
-
--- test3.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/local/bin/perl5.6.0 -w
-
-use Ace;
-
-my $db = Ace->connect(-path  => "/nfs/europa2/krb/testace/");
-
-exit(0);
-
