b4e94134fff70fe7987dd6daa10b2ad2948acd6b
keith <>
2006-04-11 21:59:39 +0000

    Rewrote main part of script, added new part to count how many sequences have each type of pentamer but not currently doing anything with this

--- count_pentamers.pl
+++ count_pentamers.pl
@@ -11,95 +11,97 @@ use strict;
 use lib "/Korflab/lib/perl";
 use FAlite;
 
+# hash to store pentamer counts, key is pentamer value is an array, first element is counts of
+# first subset of sequences, second element is count of second subset of sequences
+my %pentamer_counts;
 
 my ($file1, $file2) = @ARGV;
 
-open(OUT, ">pentamer_comparison.txt") or die;
-
-my $merFH = {};
-my $freqFH = {};
-my $merNL = {};
-my $freqNL = {};
 
 # Assume we are looking at pentamer frequencies, but could do anything
 my $window = 5;
 my $ratio;
 
 
+#########################################
+# loop through sequences in first file
+#########################################
+
 open(IN, $file1) or die "";
 my $FA = new FAlite (\*IN);
-my $counter = 0;
+my $pentamer_count1 = 0;
+my $seq_count1 = 0;
+
 while (my $entry = $FA->nextEntry) {
     my $seq = $entry->seq;
-    &mer($merFH, $seq, $window);
+    $seq_count1++;
+    
+    # now need to count pentamers
+    for (my $i = 1; $i < length($seq) - $window + 1; $i++) {
+	$pentamer_count1++;
+	my $pentamer = substr($seq, $i, $window);
+	$pentamer_counts{$pentamer}[0]++;
+    }	
+
+    # also need to keep track of how many sequences contain each type of pentamer
+    foreach my $key (keys %pentamer_counts){
+	($pentamer_counts{$key}[2]++) if((defined($pentamer_counts{$key}[0])) && ($pentamer_counts{$key}[0] > 0));
+    }
+
 }	
-close IN;
-print "Counter is $counter\n";
-
-$counter = 0;
+close(IN);
+print "$pentamer_count1 pentamers in first file ($seq_count1 sequences)\n";
 
 
+
+#########################################
+# loop through sequences in second file
+#########################################
 
 open(IN, $file2) or die "";
-my $FA2 = new FAlite (\*IN);
-while (my $entry = $FA2->nextEntry) {
+$FA = new FAlite (\*IN);
+my $pentamer_count2 = 0;
+my $seq_count2 = 0;
+
+while (my $entry = $FA->nextEntry) {
     my $seq = $entry->seq;
-    &mer($merNL, $seq, $window);
-}	
-close IN;
-
-print "Counter is $counter\n";
-
-&freq($merFH);
-&freq($merNL);
-
-#------------- print -----------------------
-print OUT "Pentamer Ratio File1 File2\n";
-
-foreach my $mFH (keys %$merFH) {
-    $ratio = ($merFH->{$mFH})/($merNL->{$mFH});
-    print (OUT "$mFH $ratio $merFH->{$mFH} $merNL->{$mFH}\n");
-#q    print (OUT "$mFH $merFH->{$mFH} $merNL->{$mFH}\n");
-}
-
-close OUT;
-
-
-# -------------SUBROUTINE -------------------------------------- 
-
-sub mer {
-    my ($merclass, $seq, $window) = @_;
+    $seq_count2++;
+    # now need to count pentamers
     for (my $i = 1; $i < length($seq) - $window + 1; $i++) {
-	my $mer = substr($seq, $i, $window);
-	$merclass->{$mer}++;
-	$counter++;
+	$pentamer_count2++;
+	my $pentamer = substr($seq, $i, $window);
+	$pentamer_counts{$pentamer}[1]++;
     }	
-}	 	
 
-sub freq {
-    my $total = 0;
-    my ($merClass) = @_;
-    foreach my $mer (sort keys %$merClass) {
-	$total += $merClass->{$mer};
+    # also need to keep track of how many sequences contain each type of pentamer
+    foreach my $key (keys %pentamer_counts){
+	($pentamer_counts{$key}[3]++) if((defined($pentamer_counts{$key}[1])) && ($pentamer_counts{$key}[1] > 0));
     }
-    foreach my $mer (sort keys %$merClass) {
-	$merClass->{$mer} /= $total;
-    }
-    print "Total is $total\n";
+}	
+close(IN);
+print "$pentamer_count2 pentamers in second file ($seq_count2 sequences)\n";
+
+
+
+##############################################################
+# Calculate frequencies of pentamers and ratio of frequencies
+# between two files
+##############################################################
+
+open(OUT, ">pentamer_comparison.txt") or die;
+
+# now need to compare ratios of pentamer frequencies
+foreach my $key (sort keys %pentamer_counts){
+    my $freq1 = $pentamer_counts{$key}[0]/$pentamer_count1;
+    my $freq2 = $pentamer_counts{$key}[1]/$pentamer_count2;
+    my $ratio = $freq1/$freq2;
+#    print "$key $pentamer_counts{$key}[2] $pentamer_counts{$key}[3]\n";
+    print OUT "$key $pentamer_counts{$key}[0] $pentamer_counts{$key}[1] $freq1 $freq2 $ratio\n";
 }
 
+close(OUT);
 
-
-
-
-
-
-
-
-
-
-
-
+exit(0);
 
 
 
