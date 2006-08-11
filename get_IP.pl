d7c70c0c079b4d0bedd40540a89f4f3d953cf319
keith <>
2006-08-11 04:00:52 +0000

    Reverted get_IP.pl script to previous simpler idea (just emails IP address and possible location).  get_desktop.pl is now the script to email screenshots.  This is because this script does not run if machine's display turns off which negates the IP address being sent.
    get_desktop.pl now just sends a screenshot and not the IP address.

--- get_IP.pl
+++ get_IP.pl
@@ -2,20 +2,15 @@
 #
 # get_IP.pl
 #
-# a script to email your IP address and include a screenshot
+# a script to email your computers IP address and possible geographic location
 #
 # by Keith Bradnam
 # July 2006
 #
-###########################################################
+################################################################################
 
 use strict;
-use MIME::Lite;
-
-# NOTES
-# i)  You must install MIME::Lite manually, it is not installed by default
-# ii) You must enable the admin group to run the screencapture command without
-#     requiring a password (use the visudo command to do this)
+use Net::SMTP;
 
 
 # 1) Get IP address
@@ -34,36 +29,19 @@ my $machine = `uname -n`;
 $machine =~ s/\.local$//;
 
 
-# 4) Create screenshot
-# -x to turn sound off
-# -m to capture main monitor
-system("sudo /usr/sbin/screencapture -x -m /tmp/screengrab.png");
+# 4) Send email
+my $smtp = Net::SMTP->new("localhost");
 
+my $from = "kbradnam\@mac.com";
+my $to = "kbradnam\@mac.com";
 
-# 5) convert to jpg.
+$smtp->mail($from);
+$smtp->to($to);
+$smtp->data();
+$smtp->datasend("Subject: MyIP: $machine - $ip - $place !!!\n");
+$smtp->dataend();
+$smtp->quit();
 
-# set jpg compression level (1=lowest quality, 100=highest quality)
-my $compression = 25;
-
-system("/usr/bin/sips -s format jpeg -s formatOptions ${compression}% --resampleWidth 1024 /tmp/screengrab.png --out /tmp/screengrab.jpg");
-
-
-# 6) create email
-
-my $msg = MIME::Lite->new( 
-			   From    =>'kbradnam@mac.com',
-			   To      =>'kbradnam@mac.com',
-			   Subject =>"MyIP: $machine - $ip - $place\n",
-			   Type    =>'image/jpg',
-			   Encoding => 'base64',
-			   Path => '/tmp/screengrab.jpg');
-        
-
-# 7) send email
-$msg->send('smtp','localhost', Debug=>0 );
-
-
-# 8) tidy up
-unlink '/tmp/screengrab.jpg','/tmp/screengrab.png';
 
 exit;
+
