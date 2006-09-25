d2b502f19a9c75b5890caf553aa1036054b48f5d
keith <>
2006-09-25 20:35:17 +0000

    New code to calculate number and percentage of N's in any assembly

--- contig_stats.pl
+++ contig_stats.pl
@@ -19,6 +19,7 @@ my $length;			# for individual contigs
 my @contig_sizes;   # lengths stored in this array 
 my $total_length; 	# sum of all contig lengths
 my $seq_count;		# count number of contigs
+my $n;				# keep track of N's in sequence
 
 SEQ: while(<>){
 	chomp;
@@ -28,6 +29,9 @@ SEQ: while(<>){
 	     	$length = length($seq);
 		 	push(@contig_sizes, $length);
 		 	$total_length += $length;
+		 	# now calculate N's in sequence (two passes to capture both lower and upper case)
+		 	$n +=  $seq =~ tr/N/N/;
+		 	$n += $seq =~ tr/n/n/;		 	
 	 	}
 		$seq = "";
 		$flag = 1;
@@ -41,7 +45,17 @@ $length = length($seq);
 push(@contig_sizes, $length);
 $total_length += $length;
 
+# now calculate N's in sequence (two passes to capture both lower and upper case)
+$n += $seq =~ tr/N/N/;
+$n += $seq =~ tr/n/n/;		 	
+
+
+
+# calculate N's as a percentage
+my $percent_N = sprintf("%.1f",$n/$total_length);
+
 print "\n$total_length - total assembly length (bp)\n";
+print "$n - N's (${percent_N}%)\n";
 print "$seq_count - number of contigs\n";
 my $mean_size = sprintf("%.1f",$total_length/$seq_count);
 print "$mean_size - average contig size (bp)\n";
