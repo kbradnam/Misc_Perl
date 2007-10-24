cbe8b90ee2fc8d6d206c98214226a9074988a53f
keith <>
2007-10-24 02:15:12 +0000

    new script to perform more focused analysis of intron sizes

---
+++ genbank_intron_analysis.pl
@@ -0,0 +1,264 @@
+#!/usr/bin/perl -w
+#
+# genbank_intron_analysis.pl
+#
+# a script to extract intron sizes for all genes in all organisms and average the size according to
+# whether it is the 1st, 2nd, 3rd, nth intron as well as non-first and last categories. Then compare
+# 1st intron sizes to other size categories
+#
+# by Keith Bradnam
+#
+# created 23rd October 2007 (but largely based on genbank_intron_size_vs_position.pl)
+#
+# Last updated by: $Author$
+# Last updated on: $Date$    
+#
+#######################################################################################
+
+
+use strict;
+use Getopt::Long;
+use List::Util qw(sum);
+
+########################
+# Command line options
+########################
+my $limit;     # set a cut-off for how many introns are needed per species (at first intron position)
+my $input;	   # the name of the intron dump file from genbank_info_dump.pl
+my $min;       # set min length for introns, any CDS with any intron below that length is ignored
+
+GetOptions("limit=i"  => \$limit,
+		   "input=s"  => \$input,
+		   "min=i"    => \$min);
+
+die "Must use -input option to specify a file containing intron info from genbank\n" if (!defined($input));
+
+#set default for $limit if not specified
+$limit  = 100   if (!defined($limit));
+
+########################
+# misc variables
+########################
+
+# simple hash for keeping track of all species: key is species name, values are just set to '1'
+my %all_species;
+
+# keep track of CDSs with introns shorter than value set by $min
+my $short_counter = 0;
+
+my %species2first_intron;
+my %species2second_intron;
+my %species2third_intron;
+my %species2fourth_intron;
+my %species2fifth_intron;
+my %species2first_intron_multi;
+my %species2last_intron;
+my %species2last_intron_multi;
+my %species2non_first_intron;
+my %species2single_intron;
+
+
+
+
+############################################################
+#
+#  M A I N   L O O P
+#
+#  process genbank dump file to put info into hashes
+#
+############################################################
+
+open (IN,"<$input") || die "Can't open $input file\n";
+
+LINE: while (<IN>) {
+	# split line and extract relevant information to separate variables
+	# need to know how long each line is to work out how many introns there are
+	chomp;
+	
+	# skip the very few occurrences of negative (or zero) sized introns and move to next line in file
+	next if (m/,-/);
+	next if (m/,0/);
+	
+	my @details = split(/,/);
+	my $length = @details;
+	my ($species,$accession,$cds) = (@details[0..2]);
+	my @introns = (@details[3..$length-1]);
+	
+	# check all intron sizes if -ignore option specified, skip CDSs with short intron
+	if($min){
+		foreach my $i (@introns){
+			if ($i < $min){
+				$short_counter++;
+				next LINE;
+			}
+		}
+	}
+	
+	# might have occasional problem with some species names in genbank
+	# names should be alphanumerical characters, spaces, and maybe a period and dash
+	next if ($species !~ m/[\w\s\.\-]+/);
+	
+	# should always have at least four entries per line 
+	next if ($length < 4);
+	
+	# keep track of species
+	$all_species{$species} = 1;
+	
+	# now add data to relevant hashes
+	my $intron = shift(@introns);
+	push(@{$species2first_intron{$species}},$intron);
+	
+	# any more introns to come? If not add to last intron hash and single intron hash
+	if(!@introns){
+		push(@{$species2last_intron{$species}},$intron);	 
+		push(@{$species2single_intron{$species}},$intron);	 
+		next LINE;
+	}
+	
+	# if we are here then there must be more introns
+	# so can also add first intron to hash of first introns in multi-intron genes
+	push(@{$species2first_intron_multi{$species}},$intron);
+
+	# get 2nd intron
+	$intron = shift(@introns);
+	push(@{$species2second_intron{$species}},$intron);
+	push(@{$species2non_first_intron{$species}},$intron);
+	
+	# any more introns to come? If not add to last intron hash (and last intron of multi-intron genes hash)
+	if(!@introns){
+		push(@{$species2last_intron{$species}},$intron);
+		push(@{$species2last_intron_multi{$species}},$intron);	
+		next LINE;
+	}
+
+	# get 3rd intron
+	$intron = shift(@introns);
+	push(@{$species2third_intron{$species}},$intron);
+	push(@{$species2non_first_intron{$species}},$intron);
+	
+	# any more introns to come? If not add to last intron hash (and last intron of multi-intron genes hash)
+	if(!@introns){
+		push(@{$species2last_intron{$species}},$intron);
+		push(@{$species2last_intron_multi{$species}},$intron);	
+		next LINE;
+	}
+	
+	# get 4th intron
+	$intron = shift(@introns);
+	push(@{$species2fourth_intron{$species}},$intron);
+	push(@{$species2non_first_intron{$species}},$intron);
+	
+	# any more introns to come? If not add to last intron hash (and last intron of multi-intron genes hash)
+	if(!@introns){
+		push(@{$species2last_intron{$species}},$intron);
+		push(@{$species2last_intron_multi{$species}},$intron);	
+		next LINE;
+	}
+
+	# get 5th intron
+	$intron = shift(@introns);
+	push(@{$species2fifth_intron{$species}},$intron);
+	push(@{$species2non_first_intron{$species}},$intron);
+	
+	# any more introns to come? If not add to last intron hash (and last intron of multi-intron genes hash)
+	if(!@introns){
+		push(@{$species2last_intron{$species}},$intron);
+		push(@{$species2last_intron_multi{$species}},$intron);	
+		next LINE;
+	}
+	
+	# not counting individual introns past 5th so can treat remainder a little differently
+	while(@introns){
+		$intron = shift(@introns);
+		push(@{$species2non_first_intron{$species}},$intron);
+		if(!@introns){
+			push(@{$species2last_intron{$species}},$intron);
+			push(@{$species2last_intron_multi{$species}},$intron);
+		}
+	}
+
+}
+
+close(IN) || die "Couldn't close $input\n";
+
+# now loop through big data structure to get stats. Species first...
+foreach my $species (sort(keys(%all_species))){
+	
+	# only want to look at species where there is some non-first intron data to compare with 1st intron
+	# i.e. skip species if there are not enough second introns (>= $limit)
+	next if (!defined(@{$species2second_intron{$species}}) || (@{$species2second_intron{$species}} < $limit));
+	
+	my ($n,$mean,$std_dev);
+	
+	print "\n=== $species ===\n";
+	
+	if(defined(@{$species2first_intron{$species}}) && (@{$species2first_intron{$species}} >= $limit)){
+		$n = @{$species2first_intron{$species}};
+		$mean = sprintf("%.2f",sum(@{$species2first_intron{$species}})/$n);
+		$std_dev = sqrt(sum(map {($_ - $mean) ** 2} @{$species2first_intron{$species}}) / ($n-1));
+		print "1st introns (all): n = $n, mean = $mean nt, sd = $std_dev\n";	
+	}
+	
+	if(defined(@{$species2first_intron_multi{$species}}) && (@{$species2first_intron_multi{$species}} >= $limit)){
+		$n = @{$species2first_intron_multi{$species}};
+		$mean = sprintf("%.2f",sum(@{$species2first_intron_multi{$species}})/$n);
+		print "1st introns (multi-intron genes): n = $n, mean = $mean nt\n";		
+	}
+
+	if(defined(@{$species2second_intron{$species}}) && (@{$species2second_intron{$species}} >= $limit)){
+		$n = @{$species2second_intron{$species}};
+		$mean = sprintf("%.2f",sum(@{$species2second_intron{$species}})/$n);
+		print "2nd introns: n = $n, mean = $mean nt\n";		
+	}
+
+	if(defined(@{$species2third_intron{$species}}) && (@{$species2third_intron{$species}} >= $limit)){
+		$n = @{$species2third_intron{$species}};
+		$mean = sprintf("%.2f",sum(@{$species2third_intron{$species}})/$n);
+		print "3rd introns: n = $n, mean = $mean nt\n";		
+	}
+	
+	if(defined(@{$species2fourth_intron{$species}}) && (@{$species2fourth_intron{$species}} >= $limit)){
+		$n = @{$species2fourth_intron{$species}};
+		$mean = sprintf("%.2f",sum(@{$species2fourth_intron{$species}})/$n);
+		print "4th introns: n = $n, mean = $mean nt\n";		
+	}
+	
+	if(defined(@{$species2fifth_intron{$species}}) && (@{$species2fifth_intron{$species}} >= $limit)){
+		$n = @{$species2fifth_intron{$species}};
+		$mean = sprintf("%.2f",sum(@{$species2fifth_intron{$species}})/$n);
+		print "5th introns: n = $n, mean = $mean nt\n";		
+	}
+	
+	if(defined(@{$species2non_first_intron{$species}}) && (@{$species2non_first_intron{$species}} >= $limit)){
+		$n = @{$species2non_first_intron{$species}};
+		$mean = sprintf("%.2f",sum(@{$species2non_first_intron{$species}})/$n);
+		# will often get data for this category but where there is not enough genes for 1st intron data,
+		# so only want to print this data if there is something that we can compare it to (1st intron from
+		# multi-intron genes)
+		print "non-1st introns: n = $n, mean = $mean nt\n" if (@{$species2first_intron_multi{$species}} >= $limit);		
+	}
+
+	if(defined(@{$species2last_intron{$species}}) && (@{$species2last_intron{$species}} >= $limit)){
+		$n = @{$species2last_intron{$species}};
+		$mean = sprintf("%.2f",sum(@{$species2last_intron{$species}})/$n);
+		print "Last introns: n = $n, mean = $mean nt\n";		
+	}
+
+	if(defined(@{$species2last_intron_multi{$species}}) && (@{$species2last_intron_multi{$species}} >= $limit)){
+		$n = @{$species2last_intron_multi{$species}};
+		$mean = sprintf("%.2f",sum(@{$species2last_intron_multi{$species}})/$n);
+		print "Last introns (multi-intron genes): n = $n, mean = $mean nt\n";		
+	}
+
+	if(defined(@{$species2single_intron{$species}}) && (@{$species2single_intron{$species}} >= $limit)){
+		$n = @{$species2single_intron{$species}};
+		$mean = sprintf("%.2f",sum(@{$species2single_intron{$species}})/$n);
+		print "Single intron genes: n = $n, mean = $mean nt\n";		
+	}
+
+}
+
+
+print "\n$short_counter CDSs with at least one introns shorter than $min bp length were ignored\n" if ($min);
+	
+exit(0);
