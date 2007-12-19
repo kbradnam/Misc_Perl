845cae066c3cebbf4150ca9b6c5a5cb5cd91c2bc
keith <>
2007-12-19 21:51:06 +0000

    New -summary function to print summary statistics of sequence lengths in input file

--- seqs2size.pl
+++ seqs2size.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl
 #
 # seqs2size.pl
 #
@@ -9,26 +9,64 @@
 ##############################################
 
 use strict;
+use warnings;
+use List::Util qw(sum);
+use Getopt::Long;
+use FAlite;
 
-my $flag;
-my $seq;
-my $length;
+# command line options
+# just one: -summary mode prints summary statistics of all sequences in a file
+my $summary; 
+GetOptions ("summary" => \$summary);
 
- SEQ: while(<>){
-	chomp;
-     if(/^>/){
-	 if($flag){
-	     $length = length($seq);
-	     print "$length\n";
-	 }
-	 $seq = "";
-	 $flag = 1;
-        next SEQ;
-     }
-     $seq .= $_;
+
+# array to store all sequence lengths
+my @sizes;
+
+open (FILE,"<$ARGV[0]") || die "Can't open file $ARGV[0]\n";
+my $fasta = new FAlite(\*FILE);
+
+# loop through each sequence in target file
+while(my $entry = $fasta->nextEntry) {
+	
+	my $seq = $entry->seq;
+    my $length = length($seq);
+	
+	# are we counting stats or just printing sequence lengths?
+	if ($summary){
+		push(@sizes,$length);
+	}
+	else{
+		print "$length\n";	
+	}
 }
+close(FILE);
 
-# get last sequence in file
-$length = length($seq);
-print "$length\n";
+# can quit unless we are in summary mode
+exit(0) unless $summary; 
 
+# calculate and print statistics
+my $n = scalar(@sizes);
+my $mean = sprintf("%.2f",sum(@sizes)/$n);
+
+# no point continuing if sample size is 1
+die "Can't calculate meaningful statistics on 1 sequence\n" if ($n == 1);
+    
+my $stdev = sprintf("%.2f",sqrt(sum(map {($_ - $mean) ** 2} @sizes) / ($n-1)));
+
+# standard error
+ my $se = sprintf("%.2f",$stdev / sqrt($n));
+
+# 95% confidence limits of the mean
+my $ci = $se * 1.96;
+
+# warn if sample size is too low to use 1.96 for calculating confidence intervals
+print "Sample size ($n) is low, calculation of 95% confidence limits will be underestimates\n\n" if ($n < 100);
+
+# print final stats.
+print "\n";
+print "N\tMean\tStd_dev\tStd_err\t95%_CI\n";
+print "$n\t$mean\t$stdev\t$se\t$ci\n\n";
+
+
+exit(0);
