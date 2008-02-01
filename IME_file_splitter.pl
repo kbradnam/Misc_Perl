8cb82e6c17ca9190395eb2ff3af3838f027638bc
keith <>
2008-02-01 02:14:19 +0000

    New options for script...still unfinished

--- IME_file_splitter.pl
+++ IME_file_splitter.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl
 #
-# split_FASTA.pl
+# IME_file_splitter.pl
 #
-# A script to separatea FASTA file into separate files based on matching a pattern in the FASTA header
+# A script to separate a FASTA file into separate files based on matching a pattern in the FASTA header
 #
 # Last updated by: $Author$
 # Last updated on: $Date$
@@ -12,12 +12,44 @@ use warnings;
 use FAlite;
 use Getopt::Long;
 
+# script can be run in two main modes 
+# i) separate whole introns into separate files based on whether the *start* of the intron is within
+#    a required range from the start of transcription (can be biased if just 1st base pair of intron 
+#    is less than threshold)
+# ii) Separate just the parts of introns that fall within in the specified size category...this might mean
+#     removing 5' and 3' ends. Can then calculate base composition on just those parts of an intron that are 
+#     a defined range from the TSS
+
+
+# command line options
+my $mode1; # see the two different descriptions above
+my $mode2; 
+my $window; # how big a size window
+my $max;    # how big to go up to
+my $step;   # for sliding windows
+
+GetOptions ("mode1" => \$mode1,
+			"mode2" => \$mode2,
+			"window=i" => \$window,
+			"max=i" => \$max,
+			"step=i" => \$step);
+
+die "Specify -mode1 or -mode2\n" if ($mode1 && $mode2);
+die "Specify -mode1 or -mode2\n" if (!$mode1 && !$mode2);
+
+# set some defaults
 my $min = 1;
-foreach my $max qw(250 500 750 1000 1250 1500 1750 2000 2250 2500 2750 3000 3250 3500 3750 4000 4250 4500 4750 5000){
+$max = 5000 if (!$max);
+$window = 250 if (!$window);
+$step = 100 if (!$step);
+my ($start,$stop);
+
+for(my $start = $min;$start<$max; $start+= $step){
+	$stop = $start + $window;
 	
-	print "Processing $min - $max\n";
+	print "Processing $start - $stop\n";
 	
-	open(OUT, ">${ARGV[1]}_${min}_${max}.fa") || die "Couldn't create output file\n";
+	open(OUT, ">${ARGV[1]}_${start}_${stop}.fa") || die "Couldn't create output file\n";
 
 	open(FILE,"<$ARGV[0]") || die "Couldn't open $ARGV[0]\n";
 
@@ -31,7 +63,7 @@ foreach my $max qw(250 500 750 1000 1250 1500 1750 2000 2250 2500 2750 3000 3250
 	   	$header =~ m/_i\d+_(\d+)_/;
 		my $distance = $1;
 		
-		if(($distance >= $min) && ($distance <= $max)){
+		if(($distance >= $start) && ($distance <= $stop)){
 			my $seq = $entry->seq;
 			print OUT "$header\n$seq\n";			
 		}
@@ -39,8 +71,6 @@ foreach my $max qw(250 500 750 1000 1250 1500 1750 2000 2250 2500 2750 3000 3250
 	}
 	close(FILE);
 	close (OUT);
-
-	$min += 250;
 }
 
 
