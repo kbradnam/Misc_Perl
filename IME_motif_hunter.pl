778fb188bb8584114ec827ec7d1d71acbfb86961
keith <>
2008-03-12 17:26:12 +0000

    Yet another IME parsing script

---
+++ IME_motif_hunter.pl
@@ -0,0 +1,130 @@
+#!/usr/bin/perl
+#
+# IME_file_splitter.pl
+#
+# A script to separate a FASTA file into separate files based on matching a pattern in the FASTA header
+#
+# Last updated by: $Author$
+# Last updated on: $Date$
+
+use strict;
+use warnings;
+use Keith;
+use FAlite;
+use Getopt::Long;
+
+# script can be run in two main modes 
+# i) SPLIT MODE - separate whole introns into separate files based on whether the *start* of the intron is within
+#    a required range from the start of transcription (can be biased if just 1st base pair of intron 
+#    is less than threshold)
+# ii) PERCENT MODE - Separate just the parts of introns that fall within in the specified size category...this might mean
+#     removing 5' and 3' ends. Can then calculate base composition on just those parts of an intron that are 
+#     a defined range from the TSS. Only show calculated percentages, don't output sequences
+
+
+# command line options
+my $split; # see the two different descriptions above
+my $percent; 
+my $window; # how big a size window
+my $max;    # how big to go up to
+my $step;   # for sliding windows
+my $prefix; # what prefix to give new file names (if using -split mode)
+
+GetOptions ("split"    => \$split,
+			"percent"  => \$percent,
+			"window=i" => \$window,
+			"max=i"    => \$max,
+			"step=i"   => \$step,
+			"prefix=s" => \$prefix);
+
+die "Specify -split or -percent\n" if ($split && $percent);
+die "Specify -split or -percent\n" if (!$split && !$percent);
+
+# set some defaults
+my $min = 1;
+$max = 5000 if (!$max);
+$window = 250 if (!$window);
+$step = 100 if (!$step);
+
+# need to know end points of each window
+my ($start,$end);
+
+# need to store merged sequences
+my $new_seq;
+
+# count how many sequences in each bin
+my $counter;
+
+for(my $start = $min;$start<$max; $start+= $step){
+	$new_seq = "";
+	$counter = 0;
+	$end = $start + $window -1;
+	
+	
+	# only write output files if in split mode
+	if($split){
+		print "Processing $start - $end\n";
+		open(OUT, ">${prefix}_${start}_${end}.fa") || die "Couldn't create output file\n";
+	}
+	
+	open(FILE,"<$ARGV[0]") || die "Couldn't open $ARGV[0]\n";
+	
+	my $fasta = new FAlite(\*FILE);
+
+	# loop through each sequence in target file
+	while(my $entry = $fasta->nextEntry) {
+	    
+		# process header
+		my $header = $entry->def;
+
+		# check whether we are working with intron or exon data
+		my $distance;
+		if($header =~ m/_i\d+_\d+_/){
+			$header =~ m/_i\d+_(\d+)_/;
+			$distance = $1;
+		}
+	   	elsif($header =~ m/_e\d+_\d+/){
+			$header =~ m/_e\d+_(\d+)/;
+			$distance = $1;
+		}
+		# check whether candidate intron falls in size category
+		if(($distance >= $start) && ($distance <= $end)){
+			$counter++;
+			my $seq = $entry->seq;
+			if ($split){ 
+				print OUT "$header\n$seq\n" 			
+			}
+			else{
+			
+				my $length = length($seq);
+
+				# if sequence is longer than $end, then just need part to extract
+				# part of it
+				if(($distance+$length) > $end){
+					my $tmp = substr($seq,1,$end-$distance);
+					$new_seq .= $tmp;
+				}
+				else{
+					$new_seq .= $seq;
+				}
+			}
+		}		
+	}
+	
+	# can now calculate base composition
+	if($percent){
+		# only calculate if there is any sequence
+		if($new_seq){
+			(my ($a,$c,$g,$t,$n,$o) = Keith::base_composition($new_seq,1));
+			print "$start,$end,$counter,$a,$c,$g,$t,$n\n";
+		}
+		else{
+			print "$start,$end,$counter,0,0,0,0,0\n";			
+		}
+	}
+	close(FILE);
+	close (OUT) if ($split);
+}
+
+
+exit(0);
