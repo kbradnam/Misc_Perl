b824acebd6c2473aef680dea713c06e5b6d65b3c
keith <>
2008-03-20 23:31:04 +0000

    new three_utr option for three prime utrs

--- IME_motif_hunter.pl
+++ IME_motif_hunter.pl
@@ -18,7 +18,7 @@ use Getopt::Long;
 # removing 5' and 3' ends. Script then take only those sequences that are a defined range from the TSS in order to
 # calculate motif density (using a NestedMICA motif)
 
-# Can currently deal with introns, exons, 5' UTRs, upstream regions of transcripts, and whole transcripts
+# Can currently deal with introns, CDSs, 5' UTRs, upstream regions of transcripts, and whole transcripts
 
 
 # command line options
@@ -29,9 +29,10 @@ my $step;       # for sliding windows
 my $motif;	    # path to a valid NestedMICA XMS motif file
 my $intron;     # path to input file of intron sequences
 my $rev_intron; # will reverse complement intron sequences
-my $exon;       # path to input file of intron sequences
+my $cds;        # path to input file of intron sequences
 my $upstream;   # path to input file up promoter regions of genes
-my $utr;	    # path to 5' UTR file
+my $five_utr;	# path to 5' UTR file
+my $three_utr;	# path to 3' UTR file
 my $transcript; # path to a file of protein-coding transcripts
 
 GetOptions ("window=i"     => \$window,
@@ -41,15 +42,16 @@ GetOptions ("window=i"     => \$window,
 			"motif=s"      => \$motif,
 			"intron=s"     => \$intron,
 			"rev_intron"   => \$rev_intron,
-			"exon=s"       => \$exon,
+			"cds=s"        => \$cds,
 			"upstream=s"   => \$upstream,
-			"utr=s"        => \$utr,
+			"five_utr=s"   => \$five_utr,
+			"three_utr=s"  => \$three_utr,
 			"transcript=s" => \$transcript);
 
 
 # check command line options 
-if (!$intron && !$exon && !$utr && !$upstream && !$transcript){
-	die "Please specify at least one of -intron, -exon, -utr, -upstream, or -transcript\n" 	
+if (!$intron && !$cds && !$five_utr && !$three_utr && !$upstream && !$transcript){
+	die "Please specify at least one of -intron, -cds, -five_utr, -three_utr, -upstream, or -transcript\n" 	
 }
 
 die "Please specify a valid NestedMica motif file with the -motif option\n" if (!$motif);
@@ -66,16 +68,18 @@ my ($start,$end);
 # hashes to store all sequences that fall in size range
 # key to hash are start coordinates, value is sequence
 my %intron2seqs;
-my %exon2seqs;
+my %cds2seqs;
 my %upstream2seqs;
-my %utr2seqs;
+my %five_utr2seqs;
+my %three_utr2seqs;
 my %transcript2seqs;
 
-# hashes to count how many introns and exons fall in each size category
+# hashes to count how many introns and CDSs fall in each size category
 my %intron2count;
-my %exon2count;
+my %cds2count;
 my %upstream2count;
-my %utr2count;
+my %five_utr2count;
+my %three_utr2count;
 my %transcript2count;
 
 for(my $start = $min;$start<$max; $start+= $step){
@@ -88,24 +92,29 @@ for(my $start = $min;$start<$max; $start+= $step){
 		$type = "intron";
 		($intron2seqs{"$start"}, $intron2count{"$start"}) = &process_sequence($intron,$type,$start,$end,$window);
 	}
-	if($exon){
-		$type = "exon";
-		($exon2seqs{"$start"}, $exon2count{"$start"}) = &process_sequence($exon,$type,$start,$end,$window);
+	if($cds){
+		$type = "cds";
+		($cds2seqs{"$start"}, $cds2count{"$start"}) = &process_sequence($cds,$type,$start,$end,$window);
 	}
 	# only have 3000 bp of upstream sequence to work with
 	if($upstream && ($end <= 3000)){
 		$type = "upstream";
 		($upstream2seqs{"$start"}, $upstream2count{"$start"}) = &process_sequence($upstream,$type,$start,$end,$window)
 	}
-	if($utr){
-		$type = "utr";
-		($utr2seqs{"$start"}, $utr2count{"$start"}) = &process_sequence($utr,$type,$start,$end,$window);
+	if($five_utr){
+		$type = "five_utr";
+		($five_utr2seqs{"$start"}, $five_utr2count{"$start"}) = &process_sequence($five_utr,$type,$start,$end,$window);
+	}
+	if($three_utr){
+		$type = "three_utr";
+		($three_utr2seqs{"$start"}, $three_utr2count{"$start"}) = &process_sequence($three_utr,$type,$start,$end,$window);
 	}
 	if($transcript){
 		$type = "transcript";
 		($transcript2seqs{"$start"}, $transcript2count{"$start"}) = &process_sequence($transcript,$type,$start,$end,$window);
 	}
 }
+
 
 ##################################################################
 #
@@ -114,11 +123,12 @@ for(my $start = $min;$start<$max; $start+= $step){
 ##################################################################
 
 print "Start,End,";
-print "Upstream_count,Upstream_bases_in_motif,Total_upstream_bases,%motif_in_upstream_region,";
-print "5UTR_count,UTR_bases_in_motif,Total_UTR_bases,%motif_in_UTR_region,";
-print "Intron_count,Intron_bases_in_motif,Total_intron_bases,%motif_in_intron,";
-print "Exon_count,Exon_bases_in_motif,Total_Exon_bases,%motif_in_exon,";
-print "Transcript_count,Transcript_bases_in_motif,Total_Transcript_bases,%motif_in_transcript\n";
+print "Upstream count,Upstream bases in motif,Total upstream bases,%motif in upstream region,";
+print "5UTR count,5UTR bases in motif,Total 5UTR bases,%motif in 5UTR region,";
+print "Intron count,Intron bases in motif,Total intron bases,%motif in intron,";
+print "CDS count,CDS bases in motif,Total CDS bases,%motif in CDS,";
+print "Transcript count,Transcript bases in motif,Total Transcript bases,%motif in transcript,";
+print "3UTR count,3UTR bases in motif,Total 3UTR bases,%motif in 3UTR region\n";
 
 # First deal with upstream sequence output (if present)
 if($upstream){
@@ -136,18 +146,18 @@ if($upstream){
 	}
 }
 
-# One big loop to treat either 5' UTR, intron, exon, or transcript sequences
+# One big loop to treat either 5' UTR, 3'UTR, intron, CDS, or transcript sequences
 for(my $start = $min;$start<$max; $start+= $step){
 	$end = $start + $window -1;
 
 	my $data;
 	print "$start,$end,0,0,0,0,";
 
-	if($utr2seqs{$start}){
-		print "$utr2count{$start},";
+	if($five_utr2seqs{$start}){
+		print "$five_utr2count{$start},";
 		open(OUT,">/tmp/ime_seq") || die "Can't write to output file\n";
 		print OUT ">${start}_$end\n";
-		print OUT "$utr2seqs{$start}\n";
+		print OUT "$five_utr2seqs{$start}\n";
 		close(OUT);
 		my $data = `~keith/Work/bin/where_is_motif.pl -species at5u -mdensity -target /tmp/ime_seq -motif $motif`;
 		$data =~ m/.*: (\d+)\/(\d+) (.*)/;
@@ -185,11 +195,11 @@ for(my $start = $min;$start<$max; $start+= $step){
 		print "0,0,0,0,";
 	}
 	
-	if($exon2seqs{$start}){
-		print "$exon2count{$start},";
+	if($cds2seqs{$start}){
+		print "$cds2count{$start},";
 		open(OUT,">/tmp/ime_seq") || die "Can't write to output file\n";
 		print OUT ">${start}_$end\n";
-		print OUT "$exon2seqs{$start}\n";
+		print OUT "$cds2seqs{$start}\n";
 		close(OUT);
 		$data = `~keith/Work/bin/where_is_motif.pl -species atc -mdensity -target /tmp/ime_seq -motif $motif`;			
 		$data =~ m/.*: (\d+)\/(\d+) (.*)/;
@@ -206,6 +216,20 @@ for(my $start = $min;$start<$max; $start+= $step){
 		print OUT "$transcript2seqs{$start}\n";
 		close(OUT);
 		$data = `~keith/Work/bin/where_is_motif.pl -species att -mdensity -target /tmp/ime_seq -motif $motif`;
+		$data =~ m/.*: (\d+)\/(\d+) (.*)/;
+		print "$1,$2,$3,";
+	}
+	else{
+		print "0,0,0,0,";
+	}
+	
+	if($three_utr2seqs{$start}){
+		print "$three_utr2count{$start},";
+		open(OUT,">/tmp/ime_seq") || die "Can't write to output file\n";
+		print OUT ">${start}_$end\n";
+		print OUT "$three_utr2seqs{$start}\n";
+		close(OUT);
+		my $data = `~keith/Work/bin/where_is_motif.pl -species at3u -mdensity -target /tmp/ime_seq -motif $motif`;
 		$data =~ m/.*: (\d+)\/(\d+) (.*)/;
 		print "$1,$2,$3,";
 	}
@@ -273,10 +297,10 @@ sub process_sequence{
 		}
 		
 		
-		# Can deal with exons, introns, and 5' UTRs and transcripts in similar ways as these all have similar 
+		# Can deal with CDSs, introns, and 5' & 3' UTRs and transcripts in similar ways as these all have similar 
 		# format FASTA headers which give the distance to the TSS (except transcripts where to the distance
 		# to the TSS is alwasy the same as $win_start)
-		elsif($type eq "intron" || $type eq "exon" || $type eq "utr"){
+		elsif($type eq "intron" || $type eq "cds" || $type eq "five_utr" || $type eq "three_utr"){
 			
 			# grab distance to TSS differently depending on what type of sequences we are dealing with
 			my $distance;
@@ -285,24 +309,28 @@ sub process_sequence{
 				$header =~ m/_i\d+_(\d+)_/;
 				$distance = $1;
 			}
-		   	elsif($type eq "exon"){
-				$header =~ m/_e\d+_(\d+)/;
+		   	elsif($type eq "cds"){
+				$header =~ m/_cds\d+_(\d+)/;
 				$distance = $1;
 			}
-			elsif($type eq "utr"){
+			elsif($type eq "five_utr"){
 				$header =~ m/_5utr\d+_(\d+)/;
 				$distance = $1;
 			}
-		
+			elsif($type eq "three_utr"){
+				$header =~ m/_3utr\d+_(\d+)/;
+				$distance = $1;
+			}
 
 			# calculate end coordinate of sequence
 			my $end_coord = $distance + $length -1;
+ 
 
 			###################################################################
 			# check whether candidate sequence falls in various size categories
 			###################################################################
 
-			# CASE 1: intron/exon/utr/transcript sequence is wholly contained within window
+			# CASE 1: intron/CDS/utr/transcript sequence is wholly contained within window
 			if(($distance >= $win_start) && ($end_coord <= $win_end)){
 				$counter++;
 				my $tmp = substr($seq,0,$end_coord-$distance+1);
