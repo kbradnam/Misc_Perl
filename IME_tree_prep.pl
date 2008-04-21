60cdd76d128a0c18336144132ea61e3c148cdb77
keith <>
2008-04-21 20:00:12 +0000

    new options for making just one type of tree at a time

--- IME_tree_prep.pl
+++ IME_tree_prep.pl
@@ -17,8 +17,35 @@ use Motif;
 ########################
 
 my $motif_dir;  # directory containing nested MICA *.xms files
+my $nm;
+my $kld;
+my $sw;
+my $pcc;
+my $ed;
+my $gapped;
+my $global;
 
-GetOptions ("motif_dir=s"  => \$motif_dir);
+GetOptions ("motif_dir=s"  => \$motif_dir,
+			"nm" => \$nm,
+			"kld" => \$kld,
+			"sw" => \$sw,
+			"pcc" => \$pcc,
+			"ed" => \$ed,
+			"gapped" => \$gapped,
+			"global" => \$global);
+
+
+die "must specify at least distance calculating method from -nm, -kld, -sw, -pcc, or -ed\n" if (!$nm && !$kld && !$sw && !$pcc && !$ed);
+die "must specify -gapped, -global or both options\n" if (!$gapped && !$global);
+
+# load methods as specified by command line options
+my @methods;
+push(@methods,"NM")  if ($nm);
+push(@methods,"KLD") if ($kld);
+push(@methods,"SW")  if ($sw);
+push(@methods,"PCC") if ($pcc);
+push(@methods,"ED")  if ($ed);
+
 
 
 # read directory containing motifs
@@ -32,11 +59,15 @@ foreach my $file (@files){
 	$motifset->nestedmica_import("$motif_dir/$file");	
 } 
 
-foreach my $method qw(NM KLD SW PCC ED){
-	$motifset->score_motifset("score"=>$method,"align"=>"global");
-	$motifset->splitstree("treefile.global.$method");
-	$motifset->score_motifset("score"=>$method,"align"=>"gapped");
-	$motifset->splitstree("treefile.gapped.$method");
+foreach my $method (@methods){
+	if($global){
+		$motifset->score_motifset("score"=>$method,"align"=>"global");
+		$motifset->splitstree("treefile.global.$method");		
+	}
+	if($gapped){
+		$motifset->score_motifset("score"=>$method,"align"=>"gapped");
+		$motifset->splitstree("treefile.gapped.$method");		
+	}
 }
 exit(0);
 
