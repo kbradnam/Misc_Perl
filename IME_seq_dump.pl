c067e41803a4144598a54eab747d2196ecfb27f1
keith <>
2009-02-13 18:03:52 +0000

    new max option to cut off introns after extracting so much sequence

--- IME_seq_dump.pl
+++ IME_seq_dump.pl
@@ -24,11 +24,13 @@ use Getopt::Long;
 my $window;     # how big a size window
 my $start;	    # start coordinate in transcript
 my $min;        # minimum amount of sequence to come from any one intron sequence (avoid taking just 1 nt of an intron)
+my $max;		# maximum amount of sequence to include (starting from 5' end)
 my $donor;		# how much to clip from 5' end of sequence
 my $acceptor;	# how much to clip from 3' end of sequence
 GetOptions ("window=i"   => \$window,
 			"start=i"	 => \$start,
 			"min=i"      => \$min,
+			"max=i"		 => \$max,
 			"donor=i"    => \$donor,  
 			"acceptor=i" => \$acceptor,
 			);
@@ -41,6 +43,8 @@ die "Specify a name of of an IME-formatted FASTA file of intron sequences\n" if 
 $start = 1     if (!$start);
 $window = 500  if (!$window);
 $min = 50      if (!$min);
+# set $max to some very large value which will never be exceeded by default (hopefully!)
+$max = 1000000 if (!$max);
 $donor = 5     if (!$donor);
 $acceptor = 10 if (!$acceptor);
 
@@ -68,7 +72,15 @@ while(my $entry = $fasta->nextEntry) {
 	# remove donor and acceptor;
 	my $seq = substr($whole_seq,$donor,-$acceptor);
 	
+	# trim sequence at 3' end if it's length is greater than $max
+	if(length($seq) > $max){
+		$seq = substr($seq,0,$max)
+	}
+	
+	# now calculate length of trimmed sequence
 	my $length = length($seq);
+
+	
 	my $header = $entry->def;
 			
 	# grab distance to TSS differently depending on what type of sequences we are dealing with
