2ce7c5502a08781577337215b1b73c2f77519a4f
keith <>
2009-07-24 02:31:52 +0000

    New script to count palindromes in sequences of a FASTA file

---
+++ palindrome.pl
@@ -0,0 +1,96 @@
+#!/usr/bin/perl
+#
+# palindrome.pl
+#
+# A script to find palindromes in a DNA sequence
+# modified from an original script written by Jules J. Berman 2002
+#
+# Last updated by: $Author$
+# Last updated on: $Date$
+
+use strict;
+use warnings;
+use Getopt::Long;
+use FAlite;
+
+########################
+# Command line options
+########################
+
+my $filename;
+my $min_loop;
+my $max_loop;
+my $min_regex;
+my $max_regex; 
+my $report;     # only show matching regexes that occur $limit times
+
+GetOptions ("filename=s"  => \$filename,
+            "min_loop=i"  => \$min_loop,
+            "max_loop=i"  => \$max_loop,
+            "min_regex=i" => \$min_regex,
+            "max_regex=i" => \$max_regex,             
+            "report=i"    => \$report
+);
+
+# set defaults 
+$min_loop  = 0  if (!$min_loop);
+$max_loop  = 15 if (!$max_loop);
+$min_regex = 5  if (!$min_regex);
+$max_regex = 20 if (!$max_regex);
+$report    = 0  if (!$report);
+
+my %palindromes;
+
+# generate list of regexes
+my @regexes;
+
+for my $n ($min_regex..$max_regex){
+	my $re = qr /[cagt]{$n}/;
+	$regexes[$n-$min_regex] = $re;
+}
+
+# keep track of number of sequences in file & total number of palindromes
+my $seq_count = 0;
+my $palindrome_count = 0;
+ 
+open (FASTA, "$filename") or die"Cannot open $filename\n";
+my $fasta = new FAlite(\*FASTA);
+
+while(my $entry = $fasta->nextEntry) {
+	$seq_count++;
+ 	my $header = $entry->def;
+    my $seq = lc($entry->seq);
+
+	# loop through each regex
+    foreach my $regex (@regexes){
+		# loop through each match to the regex
+		while ($seq =~ m/$regex/g){
+         	my $match = $&; # the match to the regex
+			my $endline = $'; # the match of everything after regex
+			
+			# make opposite pattern to match (reverse & complement)
+         	my $revmatch = reverse($match);
+         	$revmatch =~ tr/cagt/gtca/;
+			
+			# now look for match to reverse complement sequence within remainder of $seq
+			# allowing for 0-15 bases in the middle
+         	if ($endline =~ /^([cagt]{$min_loop,$max_loop})($revmatch)/){
+            	my $palindrome = uc($match) . "*" . $1 . "*" . uc($2);
+            	$palindromes{$palindrome}++;
+				$palindrome_count++;
+            }
+		}
+	}
+}
+close FASTA;
+
+
+# now loop through hash printing out final counts of all palindromes
+foreach my $key (sort {$palindromes{$a} <=> $palindromes{$b}} keys (%palindromes)) {
+	print "$key => $palindromes{$key}\n" if ($palindromes{$key} > $report);
+}
+
+my $number_of_palindromes = scalar(keys(%palindromes));
+print "$filename contains $number_of_palindromes different palindromes ($palindrome_count total) from $seq_count sequences\n";
+
+exit;
