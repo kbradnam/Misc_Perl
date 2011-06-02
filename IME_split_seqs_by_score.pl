76240ec161e9fd7facc7db9f21308246e5ac34e5
keith <>
2011-06-02 22:17:46 +0000

    Fixed script to work with slightly different output from the_imeter.pl

--- IME_split_seqs_by_score.pl
+++ IME_split_seqs_by_score.pl
@@ -2,7 +2,7 @@
 #
 # IME_split_seqs_by_score.pl
 #
-# A script to take a FASTA file of Arabidopsis introns (with IME info in header), plus a CSV file containing IMEter scores
+# A script to take a FASTA file of Arabidopsis introns (with IME info in header), plus a TSV file containing IMEter scores
 # and create new output files containing introns of fixed scores
 #
 # Last updated by: $Author$
@@ -14,24 +14,31 @@ use Keith;
 use FAlite;
 use Getopt::Long;
 
-die "Specify a CSV file and a FASTA file on the command line\n" if (@ARGV < 2);
+die "Specify a TSV file (produced by the_imeter.pl) and a FASTA file on the command line\n" if (@ARGV < 2);
 
-my ($csv,$seqs) = @ARGV;
+my ($tsv,$seqs) = @ARGV;
 
 my %intron_data;
 my %gene_data;
 
 # parse info from CSV file
-open(CSV,"<$csv") or die "Can't open $csv file\n";
-while(<CSV>){
-	next if m/^Key/; # skip header line
-	my ($id, $type, $position, $length, $distance, $ime_v1, $ime_v2, $motifs_t0, $motifs_t3, $motifs_t6) = split(/,/);
+open(TSV,"<$tsv") or die "Can't open $tsv file\n";
+while(<TSV>){
+	chomp;
+	my ($id, $ime_v2) = split(/\t/);
+	
+	my ($key, $intron, $position, $type) = $id =~ m/([A-Z0-9\.]+)_i(\d+)_(\d+)_(CDS|5UTR|3UTR)/;
+
+	# trim $id
+	$id =~ s/_\d+_(CDS|5UTR|3UTR)//;
+
 	# load data into hash, don't need everything for now
 	$intron_data{$id}{type} = $type;
 	$intron_data{$id}{position} = $position;
 	$intron_data{$id}{score} = $ime_v2;
 }
-close(CSV);
+close(TSV);
+
 
 # parse info from FASTA file
 open(FASTA,"$seqs") || die "Can't open $seqs\n";
@@ -44,14 +51,23 @@ while(my $entry = $fasta->nextEntry){
 	my ($id) = $header =~ m/>(\w+\.\d+_i\d+)_/;
 	$intron_data{$id}{seq} = $seq;
 	$intron_data{$id}{header} = $header;
+#	print "$id\n$seq\n$header\n\n";
 }
 close(FASTA);
+
 
 open(A,">at_ime_0.fa")       or die "Can't open at_ime_0.fa\n";
 open(B,">at_ime_0.1_4.9.fa")  or die "Can't open at_ime_0.1_4.9.fa\n";
 open(C,">at_ime_5_9.9.fa")   or die "Can't open at_ime_5_9.9.fa\n";
 open(D,">at_ime_10_19.9.fa") or die "Can't open at_ime_10_19.9.fa\n";
 open(E,">at_ime_20_plus.fa") or die "Can't open at_ime_20_plus.fa\n";
+
+#open(A,">at2_ime_0_1.68.fa")       or die "Can't open at_ime_0.fa\n";
+#open(B,">at2_ime_1.69_3.53.fa")  or die "Can't open at_ime_0.1_4.9.fa\n";
+#open(C,">at2_ime_3.54_5.64.fa")   or die "Can't open at_ime_5_9.9.fa\n";
+#open(D,">at2_ime_5_65_9.25.fa") or die "Can't open at_ime_10_19.9.fa\n";
+#open(E,">at2_ime_9.26_77.9.fa") or die "Can't open at_ime_20_plus.fa\n";
+
 
 foreach my $key (keys(%intron_data)){
 	my $score = $intron_data{$key}{score};
