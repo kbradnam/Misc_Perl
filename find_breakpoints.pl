25d645ff4048f50688acfe4af222cb7981b0ec8a
keith <>
2012-06-07 17:57:08 +0000

    New script to find possibe breakpoints in shattered Arabidopsis thaliana chromosomes

---
+++ find_breakpoints.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+#
+# find_breakpoints.pl
+#
+# A script to use mapping information of subreads (from BLAT) to identify possible breakpoints
+#
+# Author: Keith Bradnam, Genome Center, UC Davis
+# This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+#
+# Last updated by: $Author$
+# Last updated on: $Date$
+
+use strict;
+use warnings;
+
